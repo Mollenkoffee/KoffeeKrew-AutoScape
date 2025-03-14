49a3d30148033e1f7dc66f8758da2a6087d57e89
Aiden Trainer-Mollenkopf <atmollenkopf@gmail.com>
2025-03-14 17:07:26 -0400

    Add project files.

---
+++ KoffeeKrew-AutoScape.cpp
@@ -0,0 +1,12 @@
+﻿// KoffeeKrew-AutoScape.cpp : Defines the entry point for the application.
+//
+
+#include "KoffeeKrew-AutoScape.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
